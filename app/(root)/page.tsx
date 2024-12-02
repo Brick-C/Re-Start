e707c61c0b0b9eab579d6134a56f107eb92bedc4
TamjidBhai <tamtop2000@gmail.com>
2024-12-02 19:08:11 +0600

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  console.log("Is this sever/client side rendering?");
+
+  return (
+    <div>
+      <h1 className="text-3xl">This is NextJs-15</h1>
+    </div>
+  );
+}
