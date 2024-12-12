580426b35050ab590534fa4e0a80aa5acd937149
TamjidBhai <tamtop2000@gmail.com>
2024-12-12 23:21:05 +0600

    added style

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,21 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+const workSans = localFont({
+  src: "./fonts/WorkSans-Black.ttf",
+  weight: "900",
+  style: "normal",
 });
 
 export const metadata: Metadata = {
