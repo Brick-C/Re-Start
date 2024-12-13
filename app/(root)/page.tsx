3d027b5fdca7af02ff6aa5173757d2b48319d558
TamjidBhai <tamtop2000@gmail.com>
2024-12-13 22:15:50 +0600

    hero section incomplete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,54 @@
-export default function Home() {
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 100,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "A new way to connect with people",
+      title: "We Social",
+      category: "Social",
+      image:
+        "https://plus.unsplash.com/premium_photo-1684979565865-3539a542296e?q=80&w=2683&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
   return (
-    <div>
-      <h1 className="heading">This is NextJs-15</h1>
-    </div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your Startup,
+          <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold text-center">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
