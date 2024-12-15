6ebfd70b59ab58b216669d46589867485c2e9d77
TamjidBhai <tamtop2000@gmail.com>
2024-12-15 20:09:17 +0600

    added search

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export const experimetal_ppr = true;
+
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound();
+
+  return (
+    <>
+      <h1>This is Startup Number: {post.title}</h1>
+    </>
+  );
+};
+
+export default page;
--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  experimental: {
+    ppr: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
